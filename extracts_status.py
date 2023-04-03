baddfc6a2a51c262e276693afe709c85dcae4ee1
Polina Koriahina <109660366+PolinaKoriahina@users.noreply.github.com>
2023-04-03 16:11:44 +0300

    Update python script

--- extracts_status.py
+++ extracts_status.py
@@ -1,4 +1,9 @@
 import re
+import codecs
+
 fname = "./report_dc3078d8-ee29-4411-b4ef-fa0f0f0203f2.html"
-x = re.search("Status: (.*?)<", open(fname, 'r', encoding='utf-8').read())
-print(x.group(1))
+f=codecs.open(fname, 'r')
+print f.read()
+
+# x = re.search("Status: (.*?)<", open(fname, 'r', encoding='utf-8').read())
+# print(x.group(1))
