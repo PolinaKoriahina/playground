ae78ca4f0ec66d68e05bbd72c02fe51c955f8bb5
Polina Koriahina <109660366+PolinaKoriahina@users.noreply.github.com>
2023-04-03 22:56:45 +0300

    Update extracts_status.py

--- extracts_status.py
+++ extracts_status.py
@@ -18,10 +18,10 @@ test_sla = parsed_json["sla_test"]
 
 if test_status == "FAILED":
   color = "F35E5E"
-  image = "./red.png"
+  image = "red.png"
 else:
   color = "57C478"
-  image = "./green.png"
+  image = "green.png"
 
 sla = []
 for element in test_sla:
