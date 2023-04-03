f00ed8f63d775b44c17b605a273e755d21ce85ef
Polina Koriahina <109660366+PolinaKoriahina@users.noreply.github.com>
2023-04-03 16:14:01 +0300

    Update extracts_status.py

--- extracts_status.py
+++ extracts_status.py
@@ -3,7 +3,7 @@ import codecs
 
 fname = "./report_dc3078d8-ee29-4411-b4ef-fa0f0f0203f2.html"
 f=codecs.open(fname, 'r')
-print f.read()
+print (f.read())
 
 # x = re.search("Status: (.*?)<", open(fname, 'r', encoding='utf-8').read())
 # print(x.group(1))
