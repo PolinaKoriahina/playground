cae7174ecf893693e2958f3e7c645ad3207687a9
Polina Koriahina <109660366+PolinaKoriahina@users.noreply.github.com>
2023-04-03 22:26:03 +0300

    Update extracts_status.py

--- extracts_status.py
+++ extracts_status.py
@@ -1,9 +1,55 @@
+## imports 
 import re
-import codecs
+import json
 
-fname = "./report_dc3078d8-ee29-4411-b4ef-fa0f0f0203f2.html"
-f=codecs.open(fname, 'r')
-print (f.read())
+fname = "./raw_data_dc3078d8-ee29-4411-b4ef-fa0f0f0203f2.json"
 
-# x = re.search("Status: (.*?)<", open(fname, 'r', encoding='utf-8').read())
-# print(x.group(1))
+import json
+
+with open(fname) as user_file:
+  parsed_json = json.load(user_file)
+
+test_name = parsed_json["summary"]["name"]
+test_status = parsed_json["summary"]["qualityStatus"]
+test_start = parsed_json["summary"]["startDateText"]
+test_end = parsed_json["summary"]["endDateText"]s
+test_description = parsed_json["summary"]["description"]
+test_sla = parsed_json["sla_test"]
+
+if test_status == "FAILED":
+  color = "F35E5E"
+  image = "./red.png"
+else:
+  color = "57C478"
+  image = "./green.png"
+
+sla = []
+for element in test_sla:
+    sla.append({"name": {element["element"]["category"] + ":" + element["element"]["name"]},"value": {element["status"]}})
+
+message = {
+  "@type": "MessageCard",
+  "@context": "http://schema.org/extensions",
+  "themeColor": color,
+  "summary": test_name + ":" + test_status,
+  "sections": [
+    {
+      "activityTitle": test_name + ":" + test_status,
+      "activitySubtitle": test_start + " - " + test_end,
+      "activityImage": image,
+      "facts": [
+        {
+          "name": "Description",
+          "value": test_description
+        },
+      ]
+    },
+    {
+      "title": "SLA",
+      "facts":
+      sla
+    }
+  ]
+}
+
+print(message)
